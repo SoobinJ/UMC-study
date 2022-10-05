23489ad0e9a689b7b3c1428e73696c282bbe5e21
SoobinJ <soobin0818@konkuk.ac.kr>
2022-10-06 00:33:52 +0900

    서비스 전체보기 페이지

--- app/src/main/java/com/example/myapplication/Main2Activity.kt
+++ app/src/main/java/com/example/myapplication/Main2Activity.kt
@@ -3,7 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
